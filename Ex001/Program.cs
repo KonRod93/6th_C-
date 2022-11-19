c625b4809035c43a14431854c30b3bd74927f4d4
Konstantin <rodin.konstantin2014@yandex.ru>
2022-11-19 19:29:22 +1000

    Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры N чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите элементы, через запятую");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
